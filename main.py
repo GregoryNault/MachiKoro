968989a379982b78eb6c1ae1ed2f5e734dbe0c23
GregNault <gnault@asu.edu>
2021-12-24 06:35:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Machi Koro game
+import random
+
+
+cards = {
+    "wheat field": 1,
+    "ranch": 1,
+    "bakery": 1,
+    "cafe": 1,
+    "convenience store": 3,
+    "forest": 1,
+    "stadium": 2
+    }
+
+dice = random.randint(1, 6)
+
