6cd57368715672aed59dd7b00962f5f53ef782dd
gregn <gregnault78@gmail.com>
2022-01-01 17:35:43 -0500

    testing commit from laptop

--- money.py
+++ money.py
@@ -2,28 +2,30 @@ import cards
 
 
 def check_roll(dice, player, player_objects):
-    if dice == 1 and player.cards["wheat field"] > 0:
-        payment = cards.deck[1][-1] * player.cards["wheat field"]
-        player.coins += payment
-        print(f"You just got paid {payment} coins by your wheat field(s). Total coins: {player.coins}")
+    if dice == 1:
+        if player.cards["wheat field"] > 0:
+            payment = cards.deck[1][-1] * player.cards["wheat field"]
+            player.coins += payment
+            print(f"You just got paid {payment} coins by your wheat field(s). Total coins: {player.coins}")
 
         for other_player in player_objects:
-            if other_player != player:
+            if other_player != player and other_player.cards["wheat field"] > 0:
                 other_player_payment = cards.deck[1][-1] * other_player.cards["wheat field"]
                 other_player.coins += other_player_payment
                 print(f"{other_player.name}, just got paid {other_player_payment} for their wheat fields. Total coins: "
                       f"{other_player.coins}")
 
-    if dice == 2 and player.cards["ranch"] > 0:
-        payment = cards.deck[2][-1] * player.cards["ranch"]
-        player.coins += payment
-        print(f"You just got paid {payment} coins by your ranch(es). Total coins: {player.coins}")
+    if dice == 2:
+        if player.cards["ranch"] > 0:
+            payment = cards.deck[2][-1] * player.cards["ranch"]
+            player.coins += payment
+            print(f"You just got paid {payment} coins by your ranch(es). Total coins: {player.coins}")
 
         for other_player in player_objects:
-            if other_player != player:
-                other_player_payment = cards.deck[1][-1] * other_player.cards["wheat field"]
+            if other_player != player and other_player.cards["ranch"] > 0:
+                other_player_payment = cards.deck[1][-1] * other_player.cards["ranch"]
                 other_player.coins += other_player_payment
-                print(f"{other_player.name}, just got paid {other_player_payment} for their wheat fields. Total coins: "
+                print(f"{other_player.name}, just got paid {other_player_payment} for their ranch. Total coins: "
                       f"{other_player.coins}")
 
     if dice == 3:
@@ -44,13 +46,14 @@ def check_roll(dice, player, player_objects):
         player.coins += payment
         print(f"You just got paid {payment} coins by your convenience store(s). Total coins: {player.coins}")
 
-    if dice == 5 and player.cards["forest"] > 0:
-        payment = cards.deck[5][-1] * player.cards["forest"]
-        player.coins += payment
-        print(f"You just got paid {payment} coins by your forest(s). Total coins: {player.coins}")
+    if dice == 5:
+        if player.cards["forest"] > 0:
+            payment = cards.deck[5][-1] * player.cards["forest"]
+            player.coins += payment
+            print(f"You just got paid {payment} coins by your forest(s). Total coins: {player.coins}")
 
         for other_player in player_objects:
-            if other_player != player:
+            if other_player != player and other_player.cards["forest"]:
                 other_player_payment = cards.deck[5][-1] * other_player.cards["forest"]
                 other_player.coins += other_player_payment
                 print(f"{other_player.name}, just got paid {other_player_payment} for their forest(s). Total coins: "
@@ -66,6 +69,21 @@ def check_roll(dice, player, player_objects):
                 other_player.coins -= 1
                 player.coins += 1
                 print(f"You have received 1 coin from {other_player}")
+
+    if dice == 6 and player.cards["business center"]:
+        print("trade cards with another player")
+
+    if dice == 6 and player.cards["tv station"]:
+        other_player_selection = input("Pick another player to take coins from ['coins' to see players coins]: ").lower()
+        if other_player_selection == "coins":
+            for other_player in player_objects:
+                if other_player != player:
+                    print(f"{other_player.name} has {other_player.coins} coins.")
+        else:
+            for other_player in player_objects:
+                other_player.coins -= 5
+                player.coins += 5
+                print(f"You just took 5 coins from {other_player}")
 
     if dice == 7 and player.cards["cheese factory"] > 0:
         payment = cards.deck[7][-1] * player.cards["ranch"] * player.cards["cheese factory"]
@@ -86,25 +104,27 @@ def check_roll(dice, player, player_objects):
                 print(f"You have paid {other_player.name} a total of {payment} coins "
                       f"because of their family restaurant(s).")
 
-    if dice == 9 and player.cards["mine"] > 0:
-        payment = cards.deck[9][-1] * player.cards["mine"]
-        player.coins += payment
-        print(f"You just got paid {payment} coins by your forest(s). Total coins: {player.coins}")
+    if dice == 9:
+        if player.cards["mine"] > 0:
+            payment = cards.deck[9][-1] * player.cards["mine"]
+            player.coins += payment
+            print(f"You just got paid {payment} coins by your forest(s). Total coins: {player.coins}")
 
         for other_player in player_objects:
-            if other_player != player:
+            if other_player != player and other_player.cards["mine"] > 0:
                 other_player_payment = cards.deck[9][-1] * other_player.cards["mine"]
                 other_player.coins += other_player_payment
                 print(f"{other_player.name}, just got paid {other_player_payment} for their mine(s). Total coins: "
                       f"{other_player.coins}")
 
-    if dice == 10 and player.cards["apple orchard"] > 0:
-        payment = cards.deck[10][-1] * player.cards["apple orchard"]
-        player.coins += payment
-        print(f"You just got paid {payment} coins by your apple orchard(s). Total coins: {player.coins}")
+    if dice == 10:
+        if player.cards["apple orchard"] > 0:
+            payment = cards.deck[10][-1] * player.cards["apple orchard"]
+            player.coins += payment
+            print(f"You just got paid {payment} coins by your apple orchard(s). Total coins: {player.coins}")
 
         for other_player in player_objects:
-            if other_player != player:
+            if other_player != player and other_player["apple orchard"] > 0:
                 other_player_payment = cards.deck[10][-1] * other_player.cards["forest"]
                 other_player.coins += other_player_payment
                 print(f"{other_player.name}, just got paid {other_player_payment} for their apple orchard(s). "
